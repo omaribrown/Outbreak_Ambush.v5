db2dad335b9178986f848b807cc0dc86d9886d31
Omari Brown <omaris.brown@gmail.com>
2020-07-06 22:17:24 -0400

    created player

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ function preload() {
 }
 
 function setup() {
-
+    // creating my canvas
+    createCanvas(window.innerWidth, window.innerHeight)
+    // creating our player on the cordonates of the canvas
+    player = createVector(width/2, height/2)
+    playerSpeed = createVector(0, 0)
+    heading = 0
+    size = 60 
 }
 
 function draw() {
+    // setting background color
+    background("#00b300")
 
+    // generating and running the player
+    updatePlayer()
 }
--- player.js
+++ player.js
@@ -8,6 +8,18 @@ let heading // for direction changes
 
 let updatePlayer = () => {
 
+    // saving our current state so we can draw our character
+    push()
+    // making the center of this "drawing" to the cords of our player
+    translate(player.x, player.y)
+    // ========================== EXPLAIN HEADING IN RADIANS
+    rotate(radians(heading))
+    // making the focal point our image the center
+    imageMode(CENTER)
+    // calling our imge at 0,0 cords of our focal point
+    image(img, 0, 0, size, size)
+    // ending our drawing
+    pop()
 
 
 }
