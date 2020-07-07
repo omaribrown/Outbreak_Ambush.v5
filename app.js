a936a04ffffd0fca9de103035cda3e7de4f5292c
Omari Brown <omaris.brown@gmail.com>
2020-07-06 21:53:11 -0400

    added image to workspace

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 // setting up our p5 js structure
 function preload() {
-    
+    // loading character image used
+    img = loadImage("assets/survivor.png")
 }
 
 function setup() {
@@ -9,5 +10,5 @@ function setup() {
 }
 
 function draw() {
-    
+
 }
